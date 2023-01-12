8ce908c442ac1da6a16b2686d4e08fd4ab1c7c25
Elga <elga.mancini@gmail.com>
2023-01-12 12:04:04 +0100

    done

--- react-exercises/src/Hello.js
+++ react-exercises/src/Hello.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
-export class Hello  extends React.Component {
-    render(){
+export class Hello extends React.Component {
+  render() {
     return (
+      <div>
         <h1>Hello, World!</h1>
-    )
-}
+        <p>What a beautiful day!</p>
+      </div>
+    );
+  }
 }
