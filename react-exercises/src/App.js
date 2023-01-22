7723fd23b50615e78c03d6a781df76addeffd232
Elga <elga.mancini@gmail.com>
2023-01-22 11:44:03 +0100

    done

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Welcome } from './Welcome';
+import React from "react";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Welcome name={<strong>Elga</strong>} age={30}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Welcome name={<strong>Elga</strong>} />
+      </div>
+    );
+  }
 }
