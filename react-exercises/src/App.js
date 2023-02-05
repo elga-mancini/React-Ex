99a344c685919aba56603df21cf64c820bc2c4a6
Elga <elga.mancini@gmail.com>
2023-02-05 11:50:00 +0100

    done

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
+import { ClickTracker } from "./ClickTracker";
 import { Counter } from "./Counter";
 import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
   render() {
+    
     return (
       <div>
-        <Welcome name="John" />
-        <Counter
-          initialValue={0}
-          incrementInterval={1000}
-          incrementAmount={1}
-        />
+        <Welcome name = "John" />
+        <Counter initialValue={0} incrementInterval={1000} incrementAmount={1}/>
+        <ClickTracker />
       </div>
     );
   }
