8c5e4557ee62e765395d52e01305c6837bf16b46
Elga <elga.mancini@gmail.com>
2023-02-12 11:19:23 +0100

    done

--- react-exercises/src/InteractiveWelcome.js
+++ react-exercises/src/InteractiveWelcome.js
@@ -18,14 +18,13 @@ export class InteractiveWelcome extends React.Component {
   render() {
     return (
       <div>
-        <input name="username" value={this.state.username} onChange={this.handleInputChange}>
-        </input>
+        <input
+          name="username"
+          value={this.state.username}
+          onChange={this.handleInputChange}
+        ></input>
         <Welcome name={this.state.username} />
       </div>
     );
   }
 }
-
-// Create an InteractiveWelcome component that renders an input tag and the Welcome component.
-// Pass the current content of the input tag to the name prop of the Welcome component.
-// The input tag should be a controlled component.
