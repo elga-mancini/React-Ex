509e1ab9cd99fb9847caaaf774d43f10a5f11994
Elga <elga.mancini@gmail.com>
2023-02-12 10:13:30 +0100

    done

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { ClickTracker } from "./ClickTracker";
-import { Counter } from "./Counter";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
-import { UncontrolledLogin } from "./UncontrolledLogin";
 import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
   render() {
-    
     return (
-      <div>
-        <Welcome name = "John" />
-        <Counter initialValue={0} incrementInterval={1000} incrementAmount={1}/>
-        <ClickTracker />
-        <InteractiveWelcome name={this.props.name}/>
-        <Login />
-        <UncontrolledLogin />
+      <div className="welcome">
+        <Welcome name="Elga"/>
       </div>
     );
   }
